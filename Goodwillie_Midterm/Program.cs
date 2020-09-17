8ad4690f0d290265d43accba17e47c5d8e939c91
Jacob G <jacob.a.goodwillie@gmail.com>
2020-09-16 23:45:23 -0400

    3 questions

--- Goodwillie_Midterm/Program.cs
+++ Goodwillie_Midterm/Program.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Goodwillie_Midterm
 {
     class Program
     {
-        
+        // Name: Jacob 
+        // Date: 9/16/2020
+        // Unit 1 Test
+
+        // Recreates the 3 questions program. Will prompt user to choose a number 1-3 and display a question based on input.
+        // User has to guess correct answer within 5 seconds or else they will be asked to play again.
+
         static bool programRun = true;
         static string[] questionPrompt = { "What is your favorite color?", "What is the answer to life, the universe and everything?", "What is the airspeed velocity of an unladen swallow?"};
         static string[] answers = { "black","42" , "What do you mean? African or European swallow?" };
