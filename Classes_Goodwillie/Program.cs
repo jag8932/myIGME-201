aea2b503c1b0a52eb18b1ee57ac7c45d6b84513e
Jacob G <jacob.a.goodwillie@gmail.com>
2020-09-19 23:43:19 -0400

    PE11

---
+++ Classes_Goodwillie/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Goodwillie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+               Cat cat = new Cat("Snowball"); 
+        
+       
+        }
+    }
+
+
+
+    class Cat
+    {
+
+        public string name;
+        public int age;
+        public string color;
+
+        public Cat(string _name, int _age, string  _color)
+        {
+            name = _name;
+            age = _age;
+            color = _color;
+            whatIsCat();
+        }
+        public Cat()
+        {
+            name = "Hi";
+            age = 4;
+            color = "red";
+        }
+        public Cat(string name)
+        {
+            this.name = name;
+            age = 4;
+            color = "red";
+            whatIsCat();
+
+        }
+
+        public void whatIsCat()
+        {
+            Console.WriteLine("Its name is {0} and its age is {1}. This is a {2} colored cat.", name, age, color);
+        }
+    }
+}
---
+++ PE11_Goodwillie/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PE11_Goodwillie
+{
+    // Name: Jacob Goodwillie
+    // Date: 9/19/2020
+    /* Description: 9 classes that inherit from each other as well as interfaces. 
+    */
+    public class Vehicles
+    {
+        public virtual void LoadPassenger() { }
+    }
+
+    public class Train : Vehicles
+    {
+
+    }
+    public class Car : Vehicles
+    {
+
+    }
+
+    public interface PassengerCarrier
+    {
+        void LoadPassenger();
+    }
+
+    public interface HeavyLoadCarrier { }
+
+    public class Compact : Car {
+
+        }
+    public class SUV : Car
+    {
+
+    }
+    public class Pickup : Car
+    {
+        PassengerCarrier LoadVehicle {
+        };
+    }
+    public class PassengerTrain : Train
+    {
+
+    }
+    public class FreightTrain : Train
+    {
+
+    }
+    public class _424DoubleBogey : Train
+    {
+
+    }
+}
